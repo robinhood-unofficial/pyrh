72911c53729d984e5b59c85a5316e3c6a46674e4
Ryan D'souza <dsouzarc@gmail.com>
2015-05-05 17:44:26 -0400

    Gets username/password from file

--- example.py
+++ example.py
@@ -1,18 +1,22 @@
 from Robinhood import Robinhood
-my_trader = Robinhood(username="USERNAME HERE", password="PASSWORD HERE")
+import keyring;
+
+my_trader = Robinhood(username=keyring.get_password("system", "usernameRobinhood"), password=keyring.get_password("system", "passwordRobinhood"));
 
 #get stock information about a stock
 # Note: for some stock names, more than one instrument
 #       may be returned for a given stock symbol
 stock_instrument = my_trader.instruments("GEVO")[0]
 
+print(stock_instrument)
+
 #You can stock information about current bid_price, etc
 quote_info = my_trader.quote_data("GEVO")
 
 #place a buy order (uses market bid price)
-buy_order = my_trader.place_buy_order(stock_instrument, 1)
+#buy_order = my_trader.place_buy_order(stock_instrument, 1)
 
 #place a sell order
-sell_order = my_trader.place_sell_order(stock_instrument, 1)
+#sell_order = my_trader.place_sell_order(stock_instrument, 1)
 
 
