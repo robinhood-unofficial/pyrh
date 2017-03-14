5a9e90afeef144ea60829733eec10f926cb3c524
John Purcell <john.purcell@crowdstrike.com>
2017-03-14 16:52:42 -0700

    adding quote testing framework

--- tests/test_getdata.py
+++ tests/test_getdata.py
@@ -16,22 +16,64 @@ CONFIG = helpers.get_config(CONFIG_FILENAME)
 STOP_TEST = False
 
 TEST_QUOTE = {}
-def test_quote_endpoint(config=CONFIG):
-    """get raw data from Robinhood to test against"""
-    global TEST_QUOTE
 
-    try:
-        data = helpers.fetch_REST_directly(
-            'quotes',
-            config.get('FETCH', 'test_ticker'),
-            config
-        )
-    except Exception:
-        global STOP_TEST
-        STOP_TEST = True
-        raise
+TESTABLE_KEYS = [
+    'previous_close_date',
+    'symbol',
+    'trading_halted',
+    'previous_close',
+    'last_trade_price_source',
+    'instrument'
+]
+@pytest.mark.incremental
+class TestQuoteHelpers:
+    """wrapper to test quote architecture in order"""
+    test_ticker = CONFIG.get('FETCH', 'test_ticker')
+    fake_ticker = CONFIG.get('FETCH', 'fake_ticker')
+    def test_quote_endpoint(self, config=CONFIG):
+        """get raw data from Robinhood to test against"""
+        global TEST_QUOTE
 
-    TEST_QUOTE = data
+        try:
+            data = helpers.fetch_REST_directly(
+                'quotes',
+                self.test_ticker,
+                config
+            )
+        except Exception:
+            global STOP_TEST
+            STOP_TEST = True
+            raise
 
+        TEST_QUOTE = data
+
+    def test_validate_quote(self, config=CONFIG):
+        """validate fetcher"""
+        rh_obj = Robinhood()
+        data = rh_obj.quote_data(self.test_ticker)
+        if data['updated_at'] == TEST_QUOTE['updated_at']:
+            assert data == TEST_QUOTE
+        else:
+            for key in data.keys(): #SKIP PRICE DATA
+                if key in TESTABLE_KEYS:
+                    assert data[key] == TEST_QUOTE[key]
+
+    def test_validate_fail_quote(self, config=CONFIG):
+        """validate bad-path exception"""
+        rh_obj = Robinhood()
+        with pytest.raises(NameError):
+            data = rh_obj.quote_data(self.fake_ticker)
+
+    def test_validate_get_quote(self, config=CONFIG):
+        """validate `get_quote` call"""
+        pass
+
+    def test_validate_ask_price(self, config=CONFIG):
+        """validate `ask_price` call"""
+        pass
+
+    def test_ask_size(self, config=CONFIG):
+        """validate `ask_size` call"""
+        pass
 
 
