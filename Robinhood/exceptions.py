183b478666124a13802f286a8981934ae7ead2ec
Ryan D'souza <dsouzarc@gmail.com>
2017-10-26 22:09:24 +0200

    Uniform + consistent + spacious: documentation, follows most PEP 8 stands

--- Robinhood/exceptions.py
+++ Robinhood/exceptions.py
@@ -1,10 +1,26 @@
-"""exceptions: custom exceptions for library"""
+"""
+    Exceptions: custom exceptions for library
+"""
+
+
 class RobinhoodException(Exception):
-    """wrapper for custom Robinhood library exceptions"""
+    """
+        Wrapper for custom Robinhood library exceptions
+    """
+
     pass
+
+
 class LoginFailed(RobinhoodException):
-    """Unable to login to robinhood"""
+    """
+        Unable to login to Robinhood
+    """
     pass
+
+
 class TwoFactorRequired(LoginFailed):
-    """Unable to login thanks to 2FA failure"""
+    """
+        Unable to login because of 2FA failure
+    """
+
     pass
