87879596b50895002ed85bf61b36e1ec98a37092
aamazie <35693948+aamazie@users.noreply.github.com>
2019-04-29 13:03:00 -0400

    Added GenerateDeviceToken function
    Fixing broken log in function. Added custom device token generator

--- Robinhood/Robinhood.py
+++ Robinhood/Robinhood.py
@@ -16,6 +16,7 @@ import requests
 import six
 import dateutil
 import time
+import random
 
 #Application-specific imports
 from . import exceptions as RH_exception
@@ -154,6 +155,26 @@ class Robinhood:
         self.auth_token = None
 
         return req
+    
+    def GenerateDeviceToken():
+        rands = []
+        for i in range(0,16):
+            r = random.random()
+            rand = 4294967296.0 * r
+            rands.append((int(rand) >> ((3 & i) << 3)) & 255)
+
+        hexa = []
+        for i in range(0,256):
+            hexa.append(str(hex(i+256)).lstrip("0x").rstrip("L")[1:])
+
+        id = ""
+        for i in range(0,16):
+            id += hexa[rands[i]]
+
+            if (i == 3) or (i == 5) or (i == 7) or (i == 9):
+                id += "-"
+
+        return self.device_token = id
 
 
     ###########################################################################
