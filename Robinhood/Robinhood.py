b32b286f58557022d5d21530cb3b3387a922932f
aamazie <35693948+aamazie@users.noreply.github.com>
2019-05-05 22:46:55 -0400

    Fixed login_required indentation

--- Robinhood/Robinhood.py
+++ Robinhood/Robinhood.py
@@ -74,7 +74,7 @@ class Robinhood:
         self.password = ""
 
         def login_required(function):  # pylint: disable=E0213
-        """ Decorator function that prompts user for login if they are not logged in already. Can be applied to any function using the @ notation. """
+            """ Decorator function that prompts user for login if they are not logged in already. Can be applied to any function using the @ notation. """
             def wrapper(self, *args, **kwargs):
                 if 'Authorization' not in self.headers:
                     self.auth_method() #should this be login() ?
