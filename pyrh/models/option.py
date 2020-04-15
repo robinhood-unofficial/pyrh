fa8d01f5ce7fbd0dbe0b1ef687c86484ef499c8f
Jeff <jeffweng8@berkeley.edu>
2020-04-14 21:46:18 -0700

    fix typos, requested changes

--- pyrh/models/option.py
+++ pyrh/models/option.py
@@ -1,4 +1,4 @@
-"""Current portfolio."""
+"""Option data class"""
 
 from typing import Any
 
@@ -16,31 +16,9 @@ class Option(BaseModel):
 
 
 class OptionSchema(BaseSchema):
-    """Robinhood Option schema data loader.
-    Sample result payload from 
-    {
-        "chain_id": "cee01a93-626e-4ee6-9b04-60e2fd1392d1",
-        "chain_symbol": "AAPL",
-        "created_at": "2020-03-31T01:27:43.249339Z",
-        "expiration_date": "2020-04-17",
-        "id": "f098f169-74f9-4b91-b955-6834e1b67a12",
-        "issue_date": "2004-11-29",
-        "min_ticks": {
-            "above_tick": "0.05",
-            "below_tick": "0.01",
-            "cutoff_price": "3.00"
-        },
-        "rhs_tradability": "untradable",
-        "state": "active",
-        "strike_price": "232.5000",
-        "tradability": "tradable",
-        "type": "put",
-        "updated_at": "2020-03-31T01:27:43.249354Z",
-        "url": "https://api.robinhood.com/options/instruments/f098f169-74f9-4b91-b955-6834e1b67a12/"
-    }
-    """
+    """Robinhood Option schema data loader. """
 
-    __model__ = Options
+    __model__ = Option
 
     chain_id = fields.String()
     chain_symbol = fields.String()
@@ -55,7 +33,7 @@ class OptionSchema(BaseSchema):
     tradability = fields.String()
     options_type = fields.String()  
     updated_at = fields.NaiveDateTime()
-    url = fields.URL()  
+    url = fields.URL()
 
     @post_load
     def make_object(self, data: JSON, **kwargs: Any) -> Option:
@@ -71,10 +49,5 @@ class OptionSchema(BaseSchema):
         """
         # Can potentially move this preprocessing part to a helper file
         prefix = self.__class__.__name__.lower()
-        reserved_word_overwrite = ["type", "id"]
-        for attr in reserved_word_overwrite:
-            k, v = f"{prefix}_{attr}", data.pop("attr")
-            data[k] = v
-
         data = data.get("results", [{}])[0]
         return self.__model__(**data)
