30898f4d574af3062fbc49b017e5c168ff94b8ee
Jeff <jeffweng8@berkeley.edu>
2020-04-18 00:00:32 -0700

    fix option model, tests

--- pyrh/models/option.py
+++ pyrh/models/option.py
@@ -9,6 +9,36 @@ from pyrh.common import JSON
 from .base import BaseModel, BaseSchema
 
 
+class MinTicks(BaseModel):
+    """Min ticks data class. Describes min increments the option can be traded at."""
+
+    pass
+
+
+class MinTicksSchema(BaseSchema):
+    """Min ticks schema data loader."""
+
+    __model__ = MinTicks
+
+    above_tick = fields.Float()
+    below_tick = fields.Float()
+    cutoff_price = fields.Float()
+
+    @post_load
+    def make_object(self, data: JSON, **kwargs: Any) -> MinTicks:
+        """Build model for the Min ticks class.
+
+        Args:
+            data: The JSON diction to use to build the Min tick.
+            **kwargs: Unused but required to match signature of `Schema.make_object`
+
+        Returns:
+            An instance of the Min tick class.
+
+        """
+        return self.__model__(**data)
+
+
 class Option(BaseModel):
     """Robinhood Option data class. Represents an options instrument."""
 
@@ -22,17 +52,17 @@ class OptionSchema(BaseSchema):
 
     chain_id = fields.String()
     chain_symbol = fields.String()
-    created_at = fields.NaiveDateTime()
+    created_at = fields.DateTime()
     expiration_date = fields.Date()
     id = fields.String()
     issue_date = fields.Date()
-    min_ticks = fields.Dict()
+    min_ticks = fields.Nested(MinTicksSchema)
     rhs_tradability = fields.String()
     state = fields.String()
     strike_price = fields.Float()
     tradability = fields.String()
     type = fields.String()
-    updated_at = fields.NaiveDateTime()
+    updated_at = fields.DateTime()
     url = fields.URL()
 
     @post_load
--- tests/test_option.py
+++ tests/test_option.py
@@ -2,11 +2,11 @@ import datetime as dt
 import numbers
 from os import path
 
-import pytest
 import requests
+from dateutil import parser as p
 from six import string_types
 
-from pyrh.models.option import OptionSchema
+from pyrh.models.option import MinTicks, OptionSchema
 
 
 HERE = path.abspath(path.dirname(__file__))
@@ -17,7 +17,7 @@ sample_url = "https://api.robinhood.com/options/instruments/f098f169-74f9-4b91-b
 response = requests.get(sample_url)
 data = response.json()
 option_schema = OptionSchema()
-option_obj = option_schema.make_object(data=data)
+option_obj = option_schema.load(data)
 
 
 def test_chain_symbol():
@@ -29,26 +29,24 @@ def test_chain_symbol():
 def test_strike_price():
     strike = option_obj.strike_price
     assert isinstance(strike, numbers.Real)
-    assert format(strike, ".4f") == 232.5000
+    assert strike == 232.5
 
 
 def test_expiration_date():
     expiry = option_obj.expiration_date
-    assert isinstance(expiry, dt.datetime.date)
+    assert isinstance(expiry, dt.date)
     assert expiry == dt.date(2020, 4, 17)
 
 
 def test_created_at():
     created_at = option_obj.created_at
     assert isinstance(created_at, dt.datetime)
-    assert created_at == dt.datetime.strptime(created_at, "%Y-%m-%dT%H:%M:%S.%fZ")
+    assert created_at == p.parse("2020-03-31T01:27:43.249339Z")
 
 
 def test_min_ticks():
     min_ticks = option_obj.min_ticks
-    assert isinstance(min_ticks, dict())
-    assert min_ticks == {
-        "above_tick": "0.05",
-        "below_tick": "0.01",
-        "cutoff_price": "3.00",
-    }
+    assert isinstance(min_ticks, MinTicks)
+    assert min_ticks.above_tick == 0.05
+    assert min_ticks.below_tick == 0.01
+    assert min_ticks.cutoff_price == 3.00
