1372cb46e498b828889b5cfcb739e07806091be4
Jeff <jeffweng8@berkeley.edu>
2020-05-03 17:06:04 -0700

    merge from master, get_option_positions using model

--- pyrh/models/option.py
+++ pyrh/models/option.py
@@ -1,7 +1,8 @@
 """Option data class."""
-from typing import Any, Iterable
+from typing import Any, Iterable, cast
 
 from marshmallow import fields
+from yarl import URL
 
 from pyrh import urls
 
@@ -86,8 +87,90 @@ class OptionPaginatorSchema(BasePaginatorSchema):
     results = fields.List(fields.Nested(OptionSchema))
 
 
+class OptionPosition(BaseModel):
+    """Robinhood Option position data class. Represents an option position."""
+
+    def __repr__(self) -> str:
+        """Return the object as a string.
+
+        Returns:
+            The string representation of the object.
+
+        """
+        underlying = getattr(self, "underlying", self.option)
+        return f"OptionPosition<{underlying}|{self.quantity}|{self.type}>"
+
+
+class OptionPositionSchema(BaseSchema):
+    """Robinhood Option position schema data loader."""
+
+    __model__ = OptionPosition
+
+    account = (fields.URL(),)
+    average_price = (fields.Float(),)
+    chain_id = (fields.UUID(),)
+    chain_symbol = (fields.String(),)
+    id = (fields.UUID(),)
+    option = (fields.URL(),)
+    type = (fields.String(),)  # should this be an enum?
+    pending_buy_quantity = (fields.Float(),)
+    pending_expired_quantity = (fields.Float(),)
+    pending_expiration_quantity = (fields.Float(),)
+    pending_exercise_quantity = (fields.Float(),)
+    pending_assignment_quantity = (fields.Float(),)
+    pending_sell_quantity = (fields.Float(),)
+    quantity = (fields.Float(),)
+    intraday_quantity = (fields.Float(),)
+    intraday_average_open_price = (fields.Float(),)
+    created_at = (fields.DateTime(),)
+    trade_value_multiplier = (fields.Float(),)
+    updated_at = (fields.DateTime(),)
+    url = (fields.URL(),)
+
+
+class OptionPositionPaginator(BasePaginator):
+    """Thin wrapper around `self.results`, a list of `Option` objs."""
+
+    pass
+
+
+class OptionPositionPaginatorSchema(BasePaginatorSchema):
+    """Schema class for the OptionPaginator.
+
+    The nested results are of type `OptionPosition`.
+
+    """
+
+    __model__ = OptionPositionPaginator
+
+    results = fields.List(fields.Nested(OptionPositionSchema))
+
+
 class OptionManager(SessionManager):
     """Group together methods that manipulate an options."""
+
+    def _get_option_from_url(self, option_url: URL) -> Option:
+        """Get option from option_url.
+
+        Args:
+            option_url: url to the option, used for getting the underlying option
+                for an options position.
+
+        Returns:
+            An Option object.
+        """
+        option_data = self.get(option_url)
+        return cast(Option, OptionSchema().load(option_data))
+
+    def _get_option_positions(self, open_pos: bool = True) -> Iterable[OptionPosition]:
+        # TODO figure out what /?nonzero=true is, returns quantity = 0...
+        url = urls.OPTIONS_BASE.join(URL("positions/?nonzero=true"))
+        positions = base_paginator(url, self, OptionPositionPaginatorSchema())
+        if open_pos:
+            positions = [p for p in positions if float(p.quantity) > 0]
+        for p in positions:
+            p.underlying = self._get_option_from_url(p.option)
+        return positions
 
     def get_options(self, **kwargs: Any) -> Iterable[Option]:
         """Get a generator of options.
--- pyrh/robinhood.py
+++ pyrh/robinhood.py
@@ -1,15 +1,19 @@
 """robinhood.py: a collection of utilities for working with Robinhood's Private API."""
 
 from enum import Enum
+from typing import Iterable
 from urllib.parse import unquote
 
 import dateutil
 import requests
+from yarl import URL
 
 from pyrh import urls
 from pyrh.exceptions import InvalidTickerSymbol
 from pyrh.models import (
     InstrumentManager,
+    Option,
+    OptionManager,
     PortfolioSchema,
     SessionManager,
     SessionManagerSchema,
@@ -33,7 +37,7 @@ class Transaction(Enum):
     SELL = "sell"
 
 
-class Robinhood(InstrumentManager, SessionManager):
+class Robinhood(InstrumentManager, OptionManager, SessionManager):
     """Wrapper class for fetching/parsing Robinhood endpoints.
 
     Please see :py:class:`SessionManager` for login functionality.
@@ -82,18 +86,6 @@ class Robinhood(InstrumentManager, SessionManager):
             raise InvalidTickerSymbol()
 
         return data
-
-    def get_popularity(self, stock=""):
-        """Get the number of robinhood users who own the given stock
-        Args:
-            stock (str): stock ticker
-        Returns:
-            (int): number of users who own the stock
-        """
-        stock_instrument = self.get_url(self.quote_data(stock)["instrument"])["id"]
-        return self.get_url(urls.build_instruments(stock_instrument, "popularity"))[
-            "num_open_positions"
-        ]
 
     # We will keep for compatibility until next major release
     def quotes_data(self, stocks):
@@ -197,16 +189,13 @@ class Robinhood(InstrumentManager, SessionManager):
         if isinstance(bounds, str):  # recast to Enum
             bounds = Bounds(bounds)
 
-        historicals = (
-            urls.HISTORICALS
-            + "/?symbols="
-            + ",".join(stock).upper()
-            + "&interval="
-            + interval
-            + "&span="
-            + span
-            + "&bounds="
-            + bounds.name.lower()
+        historicals = urls.HISTORICALS.with_query(
+            [
+                ("symbols", ",".join(stock).upper()),
+                ("interval", interval),
+                ("span", span),
+                ("bounds", bounds.name.lower()),
+            ]
         )
 
         return self.get(historicals)
@@ -455,6 +444,21 @@ class Robinhood(InstrumentManager, SessionManager):
 
         return self.get(url)
 
+    def get_popularity(self, stock=""):
+        """Get the number of robinhood users who own the given stock
+
+        Args:
+            stock (str): stock ticker
+
+        Returns:
+            (int): number of users who own the stock
+
+        """
+        stock_instrument = self.get_url(self.quote_data(stock)["instrument"])["id"]
+        return self.get_url(urls.build_instruments(stock_instrument, "popularity"))[
+            "num_open_positions"
+        ]
+
     def get_tickers_by_tag(self, tag=None):
         """Get a list of instruments belonging to a tag
 
@@ -519,23 +523,30 @@ class Robinhood(InstrumentManager, SessionManager):
     #         raise InvalidOptionId()
     #     return market_data
 
+    def get_option_positions(self, open_pos: bool = True) -> Iterable[Option]:
+        # TODO figure out what /?nonzero=true is, returns quantity = 0...
+        return self._get_option_positions(open_pos)
+        # url = urls.OPTIONS_BASE.join(URL("positions/?nonzero=true"))
+        # options = base_paginator(url, self, OptionPaginatorSchema())
+        # if open_pos:
+        #     filter_ = lambda x: float(x.get("quantity")) > 0
+        #     options = [o for o in options if filter_(o)]
+        # return options
+
     def options_owned(self):
-        options = self.get_url(urls.options_base() + "positions/?nonzero=true")
+        options = self.get_url(urls.OPTIONS_BASE.join(URL("positions/?nonzero=true")))
         options = options["results"]
         return options
 
-    def get_option_marketdata(self, instrument):
-        info = self.get_url(
-            urls.build_market_data() + "options/?instruments=" + instrument
-        )
-        return info["results"][0]
+    def get_option_marketdata(self, option_id):
+        info = self.get_url(urls.MARKET_DATA_BASE.join(URL(f"options/{option_id}/")))
+        return info
 
     def get_option_chainid(self, symbol):
-        stock_info = self.get_url(self.endpoints["instruments"] + "?symbol=" + symbol)
-        stock_id = stock_info["results"][0]["id"]
-        params = {}
-        params["equity_instrument_ids"] = stock_id
-        chains = self.get_url(urls.options_base() + "chains/", params=params)
+        stock_info = self.get_url(urls.INSTRUMENTS_BASE.with_query(symbol=symbol))
+        instrument_id = stock_info["results"][0]["id"]
+        url = urls.OPTIONS_BASE.join(URL("chains/"))
+        chains = self.get_url(url.with_query(equity_instrument_ids=instrument_id))
         chains = chains["results"]
         chain_id = None
 
@@ -545,24 +556,25 @@ class Robinhood(InstrumentManager, SessionManager):
 
         return chain_id
 
-    def get_option_quote(self, arg_dict):
-        chain_id = self.get_option_chainid(arg_dict.pop("symbol", None))
-        arg_dict["chain_id"] = chain_id
-        option_info = self.get_url(
-            self.endpoints.options_base() + "instruments/", params=arg_dict
-        )
-        option_info = option_info["results"]
-        exp_price_list = []
-
-        for op in option_info:
-            mrkt_data = self.get_option_marketdata(op["url"])
-            op_price = mrkt_data["adjusted_mark_price"]
-            exp = op["expiration_date"]
-            exp_price_list.append((exp, op_price))
-
-        exp_price_list.sort()
-
-        return exp_price_list
+    def get_option_quote(self, symbol, strike, expiry, otype, state="active"):
+        url = urls.OPTIONS_BASE.join(URL("instruments/"))
+        params = {
+            "chain_symbol": symbol,
+            "strike_price": strike,
+            "expiration_dates": expiry,
+            "type": otype,
+            "state": state,
+        }
+        # symbol, strike, expiry, otype should uniquely define an option
+        results = self.get_url(url.with_query(**params)).get("results")
+        if not results:
+            return
+        else:
+            option_id = results[0]["id"]
+            result = self.get_option_marketdata(option_id)
+            params["ask"] = "{} x {}".format(result["ask_size"], result["ask_price"])
+            params["bid"] = "{} x {}".format(result["bid_size"], result["bid_price"])
+            return params
 
     ###########################################################################
     #                           GET FUNDAMENTALS
