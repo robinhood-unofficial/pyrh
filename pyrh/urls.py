087580e5fac5a53935b6177c82639eb95f4471ac
Tina Tian <31605482+Te12944265-AMAHA@users.noreply.github.com>
2022-11-25 21:55:44 -0500

    Fix bug in cancel_order due to wrong concatenation syntax (#296)
    * Fix bug in cancel_order due to wrong concatenation syntax
    * Add news fragment

--- pyrh/urls.py
+++ pyrh/urls.py
@@ -113,7 +113,7 @@ def build_orders(order_id: Optional[str] = None) -> URL:
 
     """
     if order_id is not None:
-        return ORDERS_BASE / f"/{order_id}/"
+        return ORDERS_BASE / f"{order_id}/"
     else:
         return ORDERS_BASE
 
